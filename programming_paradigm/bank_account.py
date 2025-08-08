6dc8e2c75c503d6dbc32d0c6cb0cc6b56b777025
slogan_codes <newson190@gmail.com>
2025-08-08 14:12:33 +0100

    Added the bank account oop

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,8 +3,8 @@
 
 
 class BankAccount:
-    def __init__(self):
-        self.account_balance = 0
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
 
 
     def deposit(self, amount):
