664c06d59d3bdc657e57a1fc1ca8eda7a81cd709
slogan_codes <newson190@gmail.com>
2025-08-08 15:21:25 +0100

    Added the test methods for the calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,25 +11,21 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
-        result = self.calc.add(5, 3)
-        self.assertEqual(result, 8)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(5, 3), 8)
 
     def test_minus(self):
-        result = self.calc.subtract(5, 2)
-        self.assertEqual(result, 3)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
 
     def test_multiply(self):
-        result = self.calc.multiply(5, 2)
-        self.assertEqual(result, 10)
+        self.assertEqual(self.calc.multiply(5, 3), 15)
+
 
     def test_divide(self):
-        result = self.calc.divide(10, 2)
-        self.assertEqual(result, 5)
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(10, 0), None)
 
-    def test_divide_zero(self):
-        result = self.calc.divide(10, 0)
-        self.assertEqual(result, None)
+
 
 
 
